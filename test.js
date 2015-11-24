70c395cd6e59c6d5da428933189e9c7817d44e6b
Pascal Seitz <pascal.seitz@gmail.com>
2015-11-25 00:04:41 +0100

    asd

--- test.js
+++ test.js
@@ -1,7 +1,34 @@
-var stem = require('snowball-german');
+var harmony = require('harmonyhubjs-client');
 
-console.log(stem('ausgeknipst'))
 
-console.log(stem('aktiviert'))
+var ip = '192.168.0.32';
 
-console.log(stem('abenddämmerung'));
+function turnOn(activityName) {
+    harmony(ip)
+    .then(function(harmonyClient) {
+        harmonyClient.getActivities()
+        .then(function(activities) {
+            activities.some(function(activity) {
+                // console.log(activity.label);
+                if(activity.label.toLowerCase() === activityName.toLowerCase()) {
+                    var id = activity.id;
+                    harmonyClient.startActivity(id);
+                    harmonyClient.end();
+                    return true;
+                }
+                return false;
+            });
+        });
+    });
+}
+
+function turnOff() {
+    harmony(ip)
+    .then(function(harmonyClient) {
+        console.log('Turning TV off');
+        harmonyClient.turnOff();
+        harmonyClient.end();
+    });
+}
+// turnOff();
+turnOn('Fernsehen');
